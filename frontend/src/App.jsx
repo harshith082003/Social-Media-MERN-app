90bfe2534090d91058f2d607b266ca378020068a
Harshith C S <107236868+harshith082003@users.noreply.github.com>
2023-11-04 12:10:06 +0530

    frontend react setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import './App.css'
+
+function App() {
+
+  return (
+    <Router>
+      <div>
+        hello
+      </div>
+     
+
+    </Router>
+  )
+}
+
+export default App
