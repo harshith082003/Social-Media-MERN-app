6f12f4b57bd8a0043bfaa86a4a8d8c5cbebc61c8
Harshith C S <107236868+harshith082003@users.noreply.github.com>
2024-08-11 13:34:39 +0530

    reset project

--- frontend/src/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import { userReducer } from './Reducers/User';
-
-const initialState = {}
-const store = configureStore({
-    reducer: {userReducer}
-});
-
-export default store;
